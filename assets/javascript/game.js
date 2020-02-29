15cd19e08fb86dd9592536706e264ece752eeabb
Lyyte <lyytefaridi@gmail.com>
2020-02-29 16:19:54 -0600

    set timer for 3 seconds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,24 @@ $(document).ready(function() {
 
     $("#button").on("click", function(){
         $(".container").empty();
+        
+        var time = 3;
+        var intervalid; 
+        
+        setTimeout(timeup, 1000 * 3);
+        intervalid = setInterval(timer, 1000);
 
+        function timer () {
+            time--
+            console.log(time); 
+        }
+       
         function timeup () {
-            alert("Time")
+            alert("Time");
+            clearInterval(intervalid)
         };
 
-        setTimeout(timeup, 1000 * 3)
+        
 
 
 
