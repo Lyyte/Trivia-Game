46d4933846017721231212beabe0162aef3a1ff8
Lyyte <lyytefaridi@gmail.com>
2020-02-29 18:14:13 -0600

    set the 'name' to be identical to allow different answers to not be selected at the same time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     $("#button").on("click", function(){
         $(".container").empty();
         
-        var time = 5;
+        var time = 60;
         var intervalid; 
         
-        setTimeout(timeup, 1000 * 6);
+        setTimeout(timeup, 1000 * 61);
         intervalid = setInterval(timer, 1000);
 
         function timer () {
@@ -16,10 +16,10 @@ $(document).ready(function() {
         }
 
         var answer1 = $( 
-        "<form>" + '<input type="radio" id="answer" value="answers">' + '<label for="answer">Hello0</label>' +
-        "<form>" + '<input type="radio" id="answer" value="answers">' + '<label for="answer">Hello1</label>' +
-        "<form>" + '<input type="radio" id="answer" value="answers">' + '<label for="answer">Hello2</label>' +
-        "<form>" + '<input type="radio" id="answer" value="answers">' + '<label for="answer">Hello3</label>' );
+        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer1">Hello0</label>' +
+        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer2">Hello1</label>' +
+        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer3">Hello2</label>' +
+        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer4">Hello3</label>' );
         $("#questions").append(answer1);
        
         function timeup () {
