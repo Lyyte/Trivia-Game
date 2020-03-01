c783808253ce13da347e2f728706edd9c268c921
Lyyte <lyytefaridi@gmail.com>
2020-02-29 18:58:37 -0600

    added questions, changed variable for the questions, adjusted font size for question div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,12 +15,43 @@ $(document).ready(function() {
             $(".container").html("Seconds Remaining: " + time);
         }
 
-        var answer1 = $( 
-        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer1">Hello0</label>' +
-        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer2">Hello1</label>' +
-        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer3">Hello2</label>' +
-        "<form>" + '<input type="radio" id="answer" name="q1">' + '<label for="answer4">Hello3</label>' );
-        $("#questions").append(answer1);
+        var question1 = $( 
+        "<form>Did Disney ruin Star Wars?<br>" + 
+        '<input type="radio" id="answer" name="q1" value="correct" <label for="answer1">True</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer2">False</label><br><br>');
+        $("#questions").append(question1);
+
+        var question2 = $( 
+        "<form>What is Admiral Thrawn's race?<br>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer1">Human</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer2">Miraluka</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="correct" <label for="answer3">Chiss</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer4">Cerean</label><br><br>');
+        $("#questions").append(question2);
+        
+        var question3 = $( 
+        "<form>Who Created the Rule of Two?<br>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer1">Darth Tryannus</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="corect" <label for="answer2">Darth Bane</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer3">Darth Reven</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer4">Darth Caedus</label><br><br>');
+        $("#questions").append(question3);
+
+        var question4 = $( 
+        "<form>Which lightsaber form did Vaapad derived from?<br>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer1">Niman</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="corect" <label for="answer2">Juyo</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer3">Sokan</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer4">Ataru</label><br><br>');
+        $("#questions").append(question4);
+
+        var question5 = $( 
+        "<form>Which species had invaded the known galaxy, only to be pushed back by the skywalker family?<br>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer1">Chagrians</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorect" <label for="answer2">Togrutas</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="incorrect" <label for="answer3">Mon Calamari</label>' + "<form>" + 
+        '<input type="radio" id="answer" name="q1" value="correct" <label for="answer4">Yuuzhan Vong</label><br><br>');
+        $("#questions").append(question5);
        
         function timeup () {
             alert("Time");
